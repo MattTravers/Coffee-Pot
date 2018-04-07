765a7e4ece1d8d10e1aea8b6db4f2fd822af35a7
thomaskno <tknoepff64@gmail.com>
2018-04-07 13:11:37 -0400

    added methods to the CoinSlot class

--- CoffeePot/src/coffee_pot/CoinSlot.java
+++ CoffeePot/src/coffee_pot/CoinSlot.java
@@ -1,15 +1,45 @@
 package coffee_pot;
 
 public class CoinSlot {
+	
+	
+	// in cents
+	private static int balance = 0;
+	
+	/*
+	 * 
+	 * 
+	 * list of coins for later implementation
+	 * 
+	 * 
+	 */
 
 	public boolean isEnough(int price) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	public void deduct(int price) {
-		// TODO Auto-generated method stub
-		
+	
+	
+	public insert(int amount) {
+	this.balance =+ amount;
 	}
 
+	
+	public void deduct(int price) {
+		// TODO Auto-generated method stub
+	}
+	
+	public int coinReturn() {
+		return this.balance;
+	}
+	
+	
+	// getters and setter
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	public int setBalance(int amount) {
+		this.balance = amount;
+	}
 }
