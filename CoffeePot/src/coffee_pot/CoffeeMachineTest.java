8a3967d40f7a5304da0017ba8609ce778e130ca5
Matthew Travers <matttravers@gmail.com>
2018-04-07 13:10:55 -0400

    Changes to tester and coffeeMachine

--- CoffeePot/src/coffee_pot/CoffeeMachineTest.java
+++ CoffeePot/src/coffee_pot/CoffeeMachineTest.java
@@ -7,7 +7,7 @@ class CoffeeMachineTest {
 	   @BeforeClass
 	   public static void onlyOnce()
 	   {
-
+		   CoffeeMachine coffeePot = new CoffeeMachine();
 	   }
 
 
@@ -17,45 +17,48 @@ class CoffeeMachineTest {
 
 	   }
 	  
-	   
+	   //Sally puts in one quarter, presses dispense Insufficient funds.
 	   @Test
 	   public void testA()
 	   {
 		   Assert.assertEquals();
 	   }
 
-
+	   //Sally puts in two quarters, presses dispense  Dispenses coffee.
 	   @Test
 	   public void testB()
 	   {
 		   Assert.assertEquals();
 	   }
 
-	   
+	   //Sally puts in one quarters, presses coin return Returns 25 cents.
 	   @Test
 	   public void testC()
 	   {
 		   Assert.assertEquals();
 	   }
-
+	   
+	   //Sally puts in two quarters, walks away Balance should show 50 cents.
 	   @Test
 	   public void testD()
 	   {
 		   Assert.assertEquals();
 	   }
 	   
-
+	   //Sally buys two coffees, white with sugar The sugar dispenser runs out of sugar    after the first Dispenses one coffee, displays “out of sugar.”
 	   @Test
 	   public void testE()
 	   {
 		   Assert.assertEquals();
 	   }
 	  
-
+	   //Call balance Balance should show 0 cents.
 	   @Test
 	   public void testF()
 	   {
-		   Assert.assertEquals();
+		   Assert.assertEquals(0,coffeePot.getBalance());
+		   coffeePot.addCoin(25);
+		   Assert.assertEquals(25,coffeePot.getBalance());
 	   }
 	   
 
