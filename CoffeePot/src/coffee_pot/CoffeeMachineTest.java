59af670834e0f1d2d1ad990269006e99552ed09c
thomaskno <tknoepff64@gmail.com>
2018-04-07 12:35:15 -0400

    Added Test Case Headers

--- CoffeePot/src/coffee_pot/CoffeeMachineTest.java
+++ CoffeePot/src/coffee_pot/CoffeeMachineTest.java
@@ -1,16 +1,71 @@
 package coffee_pot;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 
 class CoffeeMachineTest {
-	//setup
-	
-	//tests
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
+	   @BeforeClass
+	   public static void onlyOnce()
+	   {
+
+	   }
+
+
+	   @Before
+	   public void setUp()
+	   {
+
+	   }
+	  
+	   
+	   @Test
+	   public void testA()
+	   {
+
+	   }
+
+
+	   @Test
+	   public void testB()
+	   {
+
+	   }
+
+	   
+	   @Test
+	   public void testC()
+	   {
+
+	   }
+
+	   @Test
+	   public void testD()
+	   {
+
+	   }
+	   
+
+	   @Test
+	   public void testE()
+	   {
+
+	   }
+	  
+
+	   @Test
+	   public void testF()
+	   {
+
+	   }
+	   
+
+	   @After
+	   public void somethingThatNeedsDoingAfterEveryTestBesidesOrdinaryGarbageCollection()
+	   {
+	   }
+
+	   @AfterClass
+	   public static void somethingAfterAllIsDoneLikeLoggingOutOfDatabase()
+	   {
+	   }
 
 }
