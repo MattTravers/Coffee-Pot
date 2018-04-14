06416590007639af88f0e405b1734f4627d5c015
thomaskno <tknoepff64@gmail.com>
2018-04-14 14:14:20 -0400

    new view class

---
+++ CoffeePot/src/coffeePot/theView.java
@@ -0,0 +1,22 @@
+package coffeePot;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import javax.swing.*;
+
+
+public class theView extends JFrame{
+	
+	private JRadioButton buttons[];
+	private Container contentPane;
+	private JPanel gridPanel = new JPanel();	
+	private JPanel buttonPanel = new JPanel();	
+	private JPanel texts = new JPanel();
+	private JTextField text[];
+	
+	public theView(){
+	
+	
+	}
+}
