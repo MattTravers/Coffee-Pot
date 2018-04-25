d606578dbb4a32e3254c9174f502cf537ec97549
Matthew Travers <matttravers@gmail.com>
2018-04-25 15:21:07 -0400

    added javadoc

--- CoffeePot/src/coffeePot/Observer.java
+++ CoffeePot/src/coffeePot/Observer.java
@@ -1,7 +1,22 @@
 package coffeePot;
-
+/**
+ * Observer interface for the coffee pot project.
+ * Has three things that it wants to observe, an output, a balance, and condiment amount;
+ */
 public interface Observer {
+	/**
+	 * Used to handle output updates
+	 * @param message
+	 */
 	public void updateOutput(String message);
+	/**
+	 * Used to handle balance updates
+	 * @param message
+	 */
 	public void updateBalance(String string);
+	/**
+	 * Used to handle condiment amount updates
+	 * @param message
+	 */
 	public void updateCondiment(int n, String name);
 }
